ca0af8657bd34aba8aedfab4a3d7aa4abb3d5ed4
zsyzgu <zsyzgu@163.com>
2017-08-25 20:32:38 +0800

    Start football scene.

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -8,6 +8,17 @@ public class Data : MonoBehaviour {
     {
         public int N;
         public float[] vec;
+
+        public POS()
+        {
+
+        }
+
+        public POS(int n)
+        {
+            init(n);
+        }
+
         public void init(int N)
         {
             this.N = N;
@@ -16,11 +27,46 @@ public class Data : MonoBehaviour {
 
         public static POS operator - (POS p1, POS p2)
         {
-            for (int i = 0; i < p1.N; i++)
+            POS p = new POS(p1.N);
+            for (int i = 0; i < p.N; i++)
             {
-                p1.vec[i] -= p2.vec[i];
+                p.vec[i] = p1.vec[i] - p2.vec[i];
             }
-            return p1;
+            return p;
+        }
+
+        public static POS operator + (POS p1, POS p2)
+        {
+            POS p = new POS(p1.N);
+            for (int i = 0; i < p.N; i++)
+            {
+                p.vec[i] = p1.vec[i] + p2.vec[i];
+            }
+            return p;
+        }
+
+        public static POS operator + (POS p, Vector3 v)
+        {
+            POS ret = new POS(p.N);
+            for (int i = 0; i < ret.N; i += 3)
+            {
+                ret.vec[i + 0] = p.vec[i + 0] + v.x;
+                ret.vec[i + 1] = p.vec[i + 1] + v.y;
+                ret.vec[i + 2] = p.vec[i + 2] + v.z;
+            }
+            return ret;
+        }
+
+        public static POS operator - (POS p, Vector3 v)
+        {
+            POS ret = new POS(p.N);
+            for (int i = 0; i < ret.N; i += 3)
+            {
+                ret.vec[i + 0] = p.vec[i + 0] - v.x;
+                ret.vec[i + 1] = p.vec[i + 1] - v.y;
+                ret.vec[i + 2] = p.vec[i + 2] - v.z;
+            }
+            return ret;
         }
 
         public static float dist(POS p1, POS p2)
@@ -50,6 +96,31 @@ public class Data : MonoBehaviour {
             sum /= cnt;
             return sum;
         }
+
+        protected void formPos(List<GameObject> objs)
+        {
+            for (int i = 0; i < objs.Count; i++)
+            {
+                Vector3 pos = new Vector3();
+                Vector3 F = new Vector3();
+                Vector3 U = new Vector3();
+                if (objs[i] != null)
+                {
+                    pos = objs[i].transform.position;
+                    F = pos + objs[i].transform.forward * 0.1f;
+                    U = pos + objs[i].transform.up * 0.1f;
+                }
+                vec[i * 9 + 0] = pos.x;
+                vec[i * 9 + 1] = pos.y;
+                vec[i * 9 + 2] = pos.z;
+                vec[i * 9 + 3] = F.x;
+                vec[i * 9 + 4] = F.y;
+                vec[i * 9 + 5] = F.z;
+                vec[i * 9 + 6] = U.x;
+                vec[i * 9 + 7] = U.y;
+                vec[i * 9 + 8] = U.z;
+            }
+        }
     }
 
     public class X_POS : POS
@@ -58,6 +129,52 @@ public class Data : MonoBehaviour {
         {
             init(27);
         }
+
+        public X_POS(POS p)
+        {
+            init(27);
+            for (int i = 0; i < N; i++)
+            {
+                vec[i] = p.vec[i];
+            }
+        }
+
+        public X_POS(GameObject head, GameObject leftHand, GameObject rightHand)
+        {
+            init(27);
+            formXPos(head, leftHand, rightHand);
+        }
+
+        public void formXPos(GameObject head, GameObject leftHand, GameObject rightHand)
+        {
+            List<GameObject> objs = new List<GameObject>();
+            objs.Add(head);
+            objs.Add(leftHand);
+            objs.Add(rightHand);
+            formPos(objs);
+        }
+
+        public Vector3 getHeadPos()
+        {
+            return new Vector3(vec[0], vec[1], vec[2]);
+        }
+
+        public static float handsDist(X_POS p1, X_POS p2)
+        {
+            float sum = 0;
+            int cnt = 0;
+            for (int i = 9; i < p1.N; i += 9)
+            {
+                float d2 = 0;
+                d2 += Mathf.Pow((p1.vec[i + 0] - p1.vec[0]) - (p2.vec[i + 0] - p2.vec[0]), 2);
+                d2 += Mathf.Pow((p1.vec[i + 1] - p1.vec[1]) - (p2.vec[i + 1] - p2.vec[1]), 2);
+                d2 += Mathf.Pow((p1.vec[i + 2] - p1.vec[2]) - (p2.vec[i + 2] - p2.vec[2]), 2);
+                sum += Mathf.Sqrt(d2);
+                cnt++;
+            }
+            sum /= cnt;
+            return sum;
+        }
     }
 
     public class Y_POS : POS
@@ -65,6 +182,32 @@ public class Data : MonoBehaviour {
         public Y_POS()
         {
             init(45);
+        }
+
+        public Y_POS(POS p)
+        {
+            init(45);
+            for (int i = 0; i < N; i++)
+            {
+                vec[i] = p.vec[i];
+            }
+        }
+
+        public Y_POS(GameObject leftFoot, GameObject rightFoot, GameObject leftKnee, GameObject rightKnee, GameObject waist)
+        {
+            init(45);
+            formYPos(leftFoot, rightFoot, leftKnee, rightKnee, waist);
+        }
+
+        public void formYPos(GameObject leftFoot, GameObject rightFoot, GameObject leftKnee, GameObject rightKnee, GameObject waist)
+        {
+            List<GameObject> objs = new List<GameObject>();
+            objs.Add(leftFoot);
+            objs.Add(rightFoot);
+            objs.Add(leftKnee);
+            objs.Add(rightKnee);
+            objs.Add(waist);
+            formPos(objs);
         }
     }
 
@@ -75,29 +218,76 @@ public class Data : MonoBehaviour {
         public List<Y_POS> yPos = new List<Y_POS>();
         public X_POS xStart = new X_POS();
         public Y_POS yStart = new Y_POS();
+        public int startIndex = -1;
+        public int endIndex = -1;
 
         public void readTags(string[] tags)
         {
-            timestamp.Add(float.Parse(tags[2]));
+            timestamp.Add(float.Parse(tags[0]));
             X_POS x = new X_POS();
             for (int i = 0; i < x.N; i++)
             {
-                x.vec[i] = float.Parse(tags[3 + i]);
+                x.vec[i] = float.Parse(tags[1 + i]);
             }
             Y_POS y = new Y_POS();
             for (int i = 0; i < y.N; i++)
             {
-                y.vec[i] = float.Parse(tags[3 + x.N + i]);
+                y.vec[i] = float.Parse(tags[1 + x.N + i]);
             }
             if (timestamp.Count == 1)
             {
                 xStart = x;
                 yStart = y;
             }
-            x = (X_POS)(x - xStart);
-            y = (Y_POS)(y - yStart);
+            x = new X_POS(x - xStart);
+            y = new Y_POS(y - yStart);
             xPos.Add(x);
             yPos.Add(y);
+        }
+
+        public void segment()
+        {
+            int T = timestamp.Count;
+            int moveFrame = 0;
+            int stopFrame = 0;
+            bool moving = false;
+            const float SPEED_THRESHOLD = 0.2f;
+            const float BEGIN_DURATION = 0.1f;
+            const float END_DURATION = 0.5f;
+            for (int t = 1; t < T; t++)
+            {
+                float speed = POS.meanDist(xPos[t], xPos[t - 1]) / (timestamp[t] - timestamp[t - 1]);
+                if (speed >= SPEED_THRESHOLD)
+                {
+                    moveFrame++;
+                    stopFrame = 0;
+                    if (timestamp[t] - timestamp[t - moveFrame] >= BEGIN_DURATION)
+                    {
+                        moving = true;
+                    }
+                } else
+                {
+                    moveFrame = 0;
+                    stopFrame++;
+                    if (timestamp[t] - timestamp[t - stopFrame] >= END_DURATION)
+                    {
+                        moving = false;
+                    }
+                }
+                if (moving)
+                {
+                    if (startIndex == -1)
+                    {
+                        startIndex = t;
+                    }
+                } else
+                {
+                    if (startIndex != -1 && endIndex == -1)
+                    {
+                        endIndex = t;
+                    }
+                }
+            }
         }
     }
 
@@ -127,11 +317,10 @@ public class Data : MonoBehaviour {
             motions[name][id].readTags(tags);
         }
     }
-
-
+    
 	void Start () {
-        readFile("Data/gyz.txt");
-        Debug.Log(motions["knee_lift_left"][0].timestamp[2]);
+        //readFile("Data/gyz.txt");
+        //Debug.Log(motions["knee_lift_left"][0].timestamp[2]);
 	}
 	
 	void Update () {
---
+++ Assets/Scripts/FootController.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class FootController : MonoBehaviour {
+    public string[] motionName;
+    public GameObject head;
+    public GameObject leftHand;
+    public GameObject rightHand;
+    public GameObject leftFoot;
+    public GameObject rightFoot;
+    public GameObject leftKnee;
+    public GameObject rightKnee;
+    public GameObject waist;
+
+    class Record
+    {
+        const int RECORD_FRAMS = 10000;
+
+        private float[] timestamp = new float[RECORD_FRAMS];
+        private Data.X_POS[] xPosList = new Data.X_POS[RECORD_FRAMS];
+        private Data.Y_POS[] yPosList = new Data.Y_POS[RECORD_FRAMS];
+        private int index = -1;
+
+        public void recordData(float t, Data.X_POS xPos, Data.Y_POS yPos)
+        {
+            index++;
+            int i = index % RECORD_FRAMS;
+            timestamp[i] = t;
+            xPosList[i] = xPos;
+            yPosList[i] = yPos;
+        }
+
+        public int getIndex()
+        {
+            return index;
+        }
+
+        public float getTimestamp(int frame)
+        {
+            if (frame > index)
+            {
+                Debug.Log("Not recorded");
+                return 0;
+            }
+            return timestamp[(index - frame) % RECORD_FRAMS];
+        }
+
+        public Data.X_POS getXPos(int frame)
+        {
+            if (frame > index)
+            {
+                Debug.Log("Not recorded");
+                return new Data.X_POS();
+            }
+            return xPosList[(index - frame) % RECORD_FRAMS];
+        }
+
+        public Data.Y_POS getYPos(int frame)
+        {
+            if (frame > index)
+            {
+                Debug.Log("Not recorded");
+                return new Data.Y_POS();
+            }
+            return yPosList[(index - frame) % RECORD_FRAMS];
+        }
+    }
+    Record record = new Record();
+
+    class MovingDetect
+    {
+        const float SPEED_THRESHOLD = 0.2f;
+        const float BEGIN_DURATION = 0.1f;
+        const float END_DURATION = 0.5f;
+
+        private Vector3 lastLeftHandPos;
+        private Vector3 lastRightHandPos;
+        private int moveFrames = 0;
+        private int stopFrames = 0;
+        private bool moving = false;
+        private int startIndex = 0;
+        private Vector3 startHeadPos = new Vector3();
+
+        private void startMoving(Record record)
+        {
+            startIndex = record.getIndex() - moveFrames;
+            startHeadPos = record.getXPos(moveFrames).getHeadPos();
+        }
+
+        public bool isMoving(Record record)
+        {
+            if (record.getIndex() >= 1)
+            {
+                float speed = Data.X_POS.handsDist(record.getXPos(0), record.getXPos(1)) / (record.getTimestamp(0) - record.getTimestamp(1));
+                if (speed >= SPEED_THRESHOLD)
+                {
+                    moveFrames++;
+                    stopFrames = 0;
+                    if (record.getTimestamp(0) - record.getTimestamp(moveFrames) >= BEGIN_DURATION)
+                    {
+                        if (moving == false)
+                        {
+                            moving = true;
+                            startMoving(record);
+                        }
+                    }
+                }
+                else
+                {
+                    moveFrames = 0;
+                    stopFrames++;
+                    if (record.getTimestamp(0) - record.getTimestamp(stopFrames) >= END_DURATION)
+                    {
+                        moving = false;
+                    }
+                }
+            }
+            return moving;
+        }
+
+        public int getStartIndex()
+        {
+            return startIndex;
+        }
+
+        public Vector3 getStartHeadPos()
+        {
+            return startHeadPos;
+        }
+    }
+    MovingDetect movingDetect = new MovingDetect();
+
+    private Dictionary<string, Data.Motion> stdMotions = new Dictionary<string, Data.Motion>();
+    private void loadStdMotion()
+    {
+        foreach (string name in motionName)
+        {
+            Data.Motion motion = new Data.Motion();
+            string fileName = "Std/" + name + ".txt";
+            StreamReader sr = File.OpenText(fileName);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] tags = line.Split(' ');
+                motion.readTags(tags);
+            }
+            motion.segment();
+            stdMotions[name] = motion;
+        }
+    }
+
+    private void setLowerBody(Data.Y_POS yPos)
+    {
+        List<GameObject> objs = new List<GameObject>();
+        objs.Add(leftFoot);
+        objs.Add(rightFoot);
+        objs.Add(leftKnee);
+        objs.Add(rightKnee);
+        objs.Add(waist);
+        int cnt = 0;
+        for (int i = 0; i < yPos.N; i += 9)
+        {
+            objs[cnt].transform.position = new Vector3(yPos.vec[i + 0], yPos.vec[i + 1], yPos.vec[i + 2]);
+            objs[cnt].transform.LookAt(new Vector3(yPos.vec[i + 3], yPos.vec[i + 4], yPos.vec[i + 5]));
+            cnt++;
+        }
+    }
+
+	void Start () {
+        loadStdMotion();
+	}
+	
+	void Update () {
+        float timestamp = Time.time;
+        Data.X_POS xPos = new Data.X_POS(head, leftHand, rightHand);
+        Data.Y_POS yPos = new Data.Y_POS(leftFoot, rightFoot, leftKnee, rightKnee, waist);
+        record.recordData(timestamp, xPos, yPos);
+        bool moving = movingDetect.isMoving(record);
+        if (moving)
+        {
+            float duration = record.getTimestamp(0) - record.getTimestamp(record.getIndex() - movingDetect.getStartIndex());
+            Data.Motion motion = stdMotions["long_kick_right"];
+            int t = motion.startIndex;
+            while (t + 1 <= motion.endIndex && motion.timestamp[t + 1] - motion.timestamp[motion.startIndex] <= duration)
+            {
+                t++;
+            }
+            Data.Y_POS stdYPos = motion.yPos[t];
+            setLowerBody(new Data.Y_POS(stdYPos + motion.yStart - motion.xStart.getHeadPos() + movingDetect.getStartHeadPos()));
+        } else
+        {
+            Data.Motion motion = stdMotions["long_kick_right"];
+            setLowerBody(new Data.Y_POS(motion.yStart - motion.xStart.getHeadPos() + head.transform.position));
+        }
+	}
+}
