db7fe553fab4bf305c50bd32994434abaf761d8a
zsyzgu <zsyzgu@163.com>
2017-08-31 13:34:52 +0800

    Start to program scene 3.

---
+++ Assets/Scripts/ParkourControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkourControl : ControlledHuman {
+	new void Start () {
+        base.Start();
+
+	}
+	
+	new void Update () {
+        base.Start();
+
+	}
+}
